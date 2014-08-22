cd2cee09cd9829f4b5addaa66d72dc2c36da88df
Catherine Tran <tranc489@gmail.com>
2014-08-22 13:51:56 -0700

    add/save a donation location to the donation_locations table as well as recognize two occurrences of a donation location as two separate objects.

--- lib/donation_location.rb
+++ lib/donation_location.rb
@@ -7,6 +7,22 @@ class Donation_Location
 
   def self.all
     donation_locations = []
+    results = DB.exec("SELECT * FROM donation_locations")
+    results.each do |result|
+      attributes = {:name => result['name']}
+      current_donation_location = Donation_Location.new(attributes)
+      donation_locations << current_donation_location
+    end
+    donation_locations
   end
+
+  def save
+    results = DB.exec("INSERT INTO donation_locations (name) VALUES ('#{@name}');")
+  end
+
+  def ==(another_donation_location)
+    (self.name == another_donation_location.name)
+  end
+
 
 end
--- spec/donation_location_spec.rb
+++ spec/donation_location_spec.rb
@@ -11,5 +11,22 @@ describe Donation_Location do
     expect(test_donation_location.name).to eq 'Red Cross'
   end
 
+  it "starts out with an empty array of donation locations" do
+    test_donation_location = Donation_Location.new ({:name => 'Red Cross'})
+    expect(Donation_Location.all).to eq []
+  end
+
+  it "saves a location to the donation locations table" do
+    test_donation_location = Donation_Location.new ({:name => 'Red Cross'})
+    test_donation_location.save
+    expect(Donation_Location.all).to eq [test_donation_location]
+  end
+
+  it "recognizes two occurrences of a donation location as two separate objects" do
+    test_donation_location = Donation_Location.new ({:name => 'Red Cross'})
+    test_donation_location2 = Donation_Location.new ({:name => 'Red Cross'})
+    expect(test_donation_location).to eq test_donation_location2
+  end
+
 
 end
