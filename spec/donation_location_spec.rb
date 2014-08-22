a76e67bed23d730521dcd1051393e907a0534e81
Catherine Tran <tranc489@gmail.com>
2014-08-22 11:50:59 -0700

    make spec pass to show user the donation location they initialized.

--- spec/donation_location_spec.rb
+++ spec/donation_location_spec.rb
@@ -5,4 +5,9 @@ describe Location do
     test_location = Location.new ({:name => 'Red Cross'})
     expect(test_location).to be_an_instance_of Location
   end
+
+  it "shows the user the donation location they initialized" do
+    test_location = Location.new ({:name => 'Red Cross'})
+    expect(test_location.name).to eq 'Red Cross'
+  end
 end
