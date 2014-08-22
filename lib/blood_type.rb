707e6a94132c6de9da6c18faf373bf5a9ebf9a09
Catherine Tran <tranc489@gmail.com>
2014-08-22 14:17:49 -0700

    make initialize method in blood_type file so that a Blood type will be intiated with a name.

---
+++ lib/blood_type.rb
@@ -0,0 +1,8 @@
+class Blood_Type
+  attr_reader :name
+
+  def initialize(attributes)
+    @name = attributes[:name]
+  end
+
+end
--- spec/blood_type_spec.rb
+++ spec/blood_type_spec.rb
@@ -1,1 +1,8 @@
 require 'spec_helper'
+
+describe Blood_Type do
+  it "initializes a blood type with a name" do
+    test_blood_type = Blood_Type.new ({:name => 'B+'})
+    expect(test_blood_type).to be_an_instance_of Blood_Type
+  end
+end
