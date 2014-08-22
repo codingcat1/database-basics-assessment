a69e614fa709b0a293e971dda5c84979f26f9f30
Catherine Tran <tranc489@gmail.com>
2014-08-22 11:30:32 -0700

    add rspec configuration loop to delete data that is created so that specs do not interfere with each other.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,12 @@ require 'blood_type'
 require 'donation_location'
 require 'blood_location'
 
+DB = PG.connect ({:dbname => "test_blood_tracker_db"})
+
+RSpec.configure do |config|
+  config.before(:each) do
+    DB.exec("DELETE FROM blood_types *;")
+    DB.exec("DELETE FROM donation_locations *;")
+    DB.exec("DELETE FROM blood_locations *;")
+  end
+end
